8d349baa9ba715516fead6797b2fdf8e14d043ae
huojianpeng <2317323055@qq.com>
2017-12-29 10:35:28 +0800

    提交

---
+++ src/main/java/cn/et/Test.java
@@ -0,0 +1,17 @@
+package cn.et;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		Map map=new HashMap();
+		map.put("key", 1);
+		Map map1=new HashMap();
+		BeanUtils.copyProperties(map, map1);
+		System.out.println(map1.get("key"));
+	}
+}
